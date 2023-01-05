1a07dd6e14b7a5606bd52dbe12d3fcedcac780e9
WictorHiago <wictor.hsc@gmail.com>
2023-01-04 23:51:25 -0400

    feat page portfolio

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,22 +1,18 @@
 import React from 'react';
-import Styles from '../assets/css/Styles.css';
-import Button from '../components/Button';
+import stylePortfolio from '../assets/css/Porfolio.css';
 const Porfolio = () => {
-  return (
-    <>
-      <div className="cards-container-port" style={Styles}>
-        <div className="card-box-port"></div>
-        <div className="card-box-port"></div>
-        <div className="card-box-port"></div>
-        <div className="card-box-port"></div>
-        <div className="card-box-port"></div>
-        <div className="card-box-port"></div>
+   return (
+      <div className="portfolio-container" style={stylePortfolio}>
+         <h2>Em construçao</h2>
+         <br />
+         <a
+            href="https://github.com/WictorHiago/crud-api-sequelize/blob/main/README.md"
+            target={'blank'}
+         >
+            Repositório github
+         </a>
       </div>
-      <div>
-        <Button />
-      </div>
-    </>
-  );
+   );
 };
 
 export default Porfolio;
